59f51b722f4b761ae0a232d99d439f94a25bea6b
Kaijia Xu <kx233@cam.ac.uk>
2023-11-15 16:51:39 +0000

    Q1

--- fynesse/access.py
+++ fynesse/access.py
@@ -3,6 +3,7 @@ import yaml
 from ipywidgets import interact_manual, Text, Password
 import pymysql
 import urllib.request
+import pandas as pd
 
 """These are the types of import we might expect in this file
 import httplib2
@@ -51,6 +52,14 @@ def create_database_connection(user, password, host, database, port=3306):
         print(f"Error connecting to the MariaDB Server: {e}")
     return conn
 
+def load_ppdata_csvs(from_year, to_year):
+    """
+    Download Property Price Data 
+    """
+    for year in range(from_year, to_year + 1):
+        for partnumber in range(1, 3):
+            urllib.request.urlretrieve('http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-{year}-part{partnumber}.csv'.format(year=year, partnumber=partnumber), 'pp-{year}-part{partnumber}.csv'.format(year=year, partnumber=partnumber))
+
 def run_query(query):
     """
     Run the given query on the database
@@ -68,7 +77,176 @@ def run_query(query):
     conn.commit()
     cur.close()
 
-def load_ppdata_csvs(from_year, to_year):
-    for year in range(from_year, to_year + 1):
-        for partnumber in range(1, 3):
-            urllib.request.urlretrieve('http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-{year}-part{partnumber}.csv'.format(year=year, partnumber=partnumber), 'pp-{year}-part{partnumber}.csv'.format(year=year, partnumber=partnumber))
+def run_query_return_results(query):
+    """
+    Run the given query on the database and return results
+    """
+    with open("credentials.yaml") as file:
+        credentials = yaml.safe_load(file)
+    with open("database-details.yaml") as file:
+        database_details = yaml.safe_load(file)
+    username = credentials["username"]
+    password = credentials["password"]
+    url = database_details["database"]
+    conn = create_database_connection(user=username, password=password, host=url, database="property_prices")
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    return rows
+
+
+def create_pp_data():
+    """
+    Create the schema for pp_data, including adding db_id.
+    """
+    schema = ["DROP TABLE IF EXISTS `pp_data`;",    
+            "CREATE TABLE IF NOT EXISTS `pp_data` (",
+            "`transaction_unique_identifier` tinytext COLLATE utf8_bin NOT NULL,",
+            "`price` int(10) unsigned NOT NULL,",
+            "`date_of_transfer` date NOT NULL,",
+            "`postcode` varchar(8) COLLATE utf8_bin NOT NULL,",
+            "`property_type` varchar(1) COLLATE utf8_bin NOT NULL,",
+            "`new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,",
+            "`tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,",
+            "`primary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,",
+            "`secondary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,",
+            "`street` tinytext COLLATE utf8_bin NOT NULL,",
+            "`locality` tinytext COLLATE utf8_bin NOT NULL,",
+            "`town_city` tinytext COLLATE utf8_bin NOT NULL,",
+            "`district` tinytext COLLATE utf8_bin NOT NULL,",
+            "`county` tinytext COLLATE utf8_bin NOT NULL,",
+            "`ppd_category_type` varchar(2) COLLATE utf8_bin NOT NULL,",
+            "`record_status` varchar(2) COLLATE utf8_bin NOT NULL,",
+            "`db_id` bigint(20) unsigned NOT NULL"
+            ") DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;",
+            "ALTER TABLE `pp_data` ADD PRIMARY KEY (`db_id`);",
+            "ALTER TABLE `pp_data` MODIFY `db_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;"]
+    for line in schema:
+        run_query(line)
+
+
+def create_postcode_data():
+    """
+    Create the schema for postcode_data, including adding db_id.
+    """
+    schema = ["DROP TABLE IF EXISTS `postcode_data`;",
+              "CREATE TABLE IF NOT EXISTS `postcode_data` (",
+              "`postcode` varchar(8) COLLATE utf8_bin NOT NULL,",
+              "`status` enum('live','terminated') NOT NULL,",
+              "`usertype` enum('small', 'large') NOT NULL,",
+              "`easting` int unsigned,",
+              "`northing` int unsigned,",
+              "`positional_quality_indicator` int NOT NULL,",
+              "`country` enum('England', 'Wales', 'Scotland', 'Northern Ireland', 'Channel Islands', 'Isle of Man') NOT NULL,",
+              "`latitude` decimal(11,8) NOT NULL,",
+              "`longitude` decimal(10,8) NOT NULL,",
+              "`postcode_no_space` tinytext COLLATE utf8_bin NOT NULL,",
+              "`postcode_fixed_width_seven` varchar(7) COLLATE utf8_bin NOT NULL,",
+              "`postcode_fixed_width_eight` varchar(8) COLLATE utf8_bin NOT NULL,",
+              "`postcode_area` varchar(2) COLLATE utf8_bin NOT NULL,",
+              "`postcode_district` varchar(4) COLLATE utf8_bin NOT NULL,",
+              "`postcode_sector` varchar(6) COLLATE utf8_bin NOT NULL,",
+              "`outcode` varchar(4) COLLATE utf8_bin NOT NULL,",
+              "`incode` varchar(3)  COLLATE utf8_bin NOT NULL,",
+              "`db_id` bigint(20) unsigned NOT NULL",
+              ") DEFAULT CHARSET=utf8 COLLATE=utf8_bin;",
+              "ALTER TABLE `postcode_data` ADD PRIMARY KEY (`db_id`);",
+              "ALTER TABLE `postcode_data` MODIFY `db_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;"]
+    for line in schema:
+        run_query(line)
+
+
+def create_indices():
+    """
+    Index both the `pp_data` and `postcode_data` table.
+    I suspect only the ppdata_date_of_transfer, ppdata_postcode and postcodedata_postcode are needed, but I created indices for all fields in `prices_coordinates_data`.
+    """
+    query = [
+        "CREATE INDEX ppdata_price ON pp_data (price);",
+        "CREATE INDEX ppdata_date_of_transfer ON pp_data (date_of_transfer);",
+        "CREATE INDEX ppdata_property_type ON pp_data (property_type);",
+        "CREATE INDEX ppdata_new_build_flag ON pp_data (new_build_flag);",
+        "CREATE INDEX ppdata_tenure_type ON pp_data (tenure_type);",
+        "CREATE INDEX ppdata_locality ON pp_data (locality);",
+        "CREATE INDEX ppdata_town_city ON pp_data (town_city);",
+        "CREATE INDEX ppdata_district ON pp_data (district);",
+        "CREATE INDEX ppdata_county ON pp_data (county);",
+        "CREATE INDEX ppdata_postcode ON pp_data (postcode);",
+        "CREATE INDEX postcodedata_postcode ON postcode_data (postcode);",
+        "CREATE INDEX postcodedata_country ON postcode_data (country);",
+        "CREATE INDEX postcodedata_latitude ON postcode_data (latitude);",
+        "CREATE INDEX postcodedata_longitude ON postcode_data (longitude);"
+    ]
+    for line in query:
+        run_query(line)
+
+
+def create_indices():
+    """
+    Index both the `pp_data` and `postcode_data` table.
+    I suspect only the ppdata_date_of_transfer, ppdata_postcode and postcodedata_postcode are needed, but I created indices for all fields in `prices_coordinates_data`.
+    """
+    query = [
+        "CREATE INDEX ppdata_price ON pp_data (price);",
+        "CREATE INDEX ppdata_date_of_transfer ON pp_data (date_of_transfer);",
+        "CREATE INDEX ppdata_property_type ON pp_data (property_type);",
+        "CREATE INDEX ppdata_new_build_flag ON pp_data (new_build_flag);",
+        "CREATE INDEX ppdata_tenure_type ON pp_data (tenure_type);",
+        "CREATE INDEX ppdata_locality ON pp_data (locality);",
+        "CREATE INDEX ppdata_town_city ON pp_data (town_city);",
+        "CREATE INDEX ppdata_district ON pp_data (district);",
+        "CREATE INDEX ppdata_county ON pp_data (county);",
+        "CREATE INDEX ppdata_postcode ON pp_data (postcode);",
+        "CREATE INDEX postcodedata_postcode ON postcode_data (postcode);",
+        "CREATE INDEX postcodedata_country ON postcode_data (country);",
+        "CREATE INDEX postcodedata_latitude ON postcode_data (latitude);",
+        "CREATE INDEX postcodedata_longitude ON postcode_data (longitude);"
+    ]
+    for line in query:
+        run_query(line)
+
+
+def join_one_year(year):
+    """
+    Join by year, save results into a csv
+    """
+    # For readability, join_query was separated:
+    join_query = ["SELECT * FROM",
+                  f"(SELECT `price`, `date_of_transfer`, `postcode`, `property_type`, `new_build_flag`, `tenure_type`, `locality`, `town_city`, `district`, `county`, `db_id` FROM `pp_data` WHERE date_of_transfer >= '{year}-01-01' AND date_of_transfer <= '{year}-12-31') pp_data_temp",
+                  "INNER JOIN",
+                  "(SELECT `country`, `latitude`, `longitude`, `postcode` FROM `postcode_data`) postcode_data_temp",
+                  "ON pp_data_temp.`postcode` = postcode_data_temp.`postcode`"]
+    join_query = join_query.join(" ")
+    results = run_query_return_results(join_query)
+    df = pd.DataFrame(results)
+    df.to_csv(f'joined-{year}.csv', index=False)
+    print('Successfully joined {year}\n')
+    return results
+
+
+def create_prices_coordinates_data():
+    """
+    Create the schema for prices_coordinates_data.
+    """
+    schema = ["DROP TABLE IF EXISTS `prices_coordinates_data`;",
+              "CREATE TABLE IF NOT EXISTS `prices_coordinates_data` (",
+              "`price` int(10) unsigned NOT NULL,",
+              "`date_of_transfer` date NOT NULL,",
+              "`postcode` varchar(8) COLLATE utf8_bin NOT NULL,",
+              "`property_type` varchar(1) COLLATE utf8_bin NOT NULL,",
+              "`new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,",
+              "`tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,",
+              "`locality` tinytext COLLATE utf8_bin NOT NULL,",
+              "`town_city` tinytext COLLATE utf8_bin NOT NULL,",
+              "`district` tinytext COLLATE utf8_bin NOT NULL,",
+              "`county` tinytext COLLATE utf8_bin NOT NULL,",
+              "`country` enum('England', 'Wales', 'Scotland', 'Northern Ireland', 'Channel Islands', 'Isle of Man') NOT NULL,",
+              "`latitude` decimal(11,8) NOT NULL,",
+              "`longitude` decimal(10,8) NOT NULL,",
+              "`db_id` bigint(20) unsigned NOT NULL",
+              ") DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;"]
+    for line in schema:
+        run_query(line)
+
+def load_data(filename, tablename):
+    run_query(f"""LOAD DATA LOCAL INFILE '{filename}' INTO TABLE `{tablename}` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED by '"' LINES STARTING BY '' TERMINATED BY '\n';""")
